f6fff29ff63ca846b004ebf90456640241aca9e8
alexisstdev <alexissanmiguel03@gmail.com>
2023-05-30 10:31:46 -0500

    add weather app

--- src/WorksData.js
+++ src/WorksData.js
@@ -1,29 +1,41 @@
 export const worksData = [
-  {
-    id: 1,
-    name: 'Alexis.AI - Image Generation',
-    path: 'alexisai',
-    category: 'Fullstack Application',
-    images: ['/works/alexis-ai/1.png', '/works/alexis-ai/2.png'],
-    preview: '/works/alexis-ai/1.png',
-    description: 'A tool for generate and share unique images with DALL-E.',
-    tags: ['React', 'Node.js', 'Express', 'MongoDB', 'DALL-E'],
-    github: 'https://github.com/alexisstdev/alexis.ai',
-    live: 'https://alexis-ai.vercel.app/',
-    longDescription:
-      'Fullstack MERN application, this personal project is an image generation tool that utilizes DALL-E to create unique and beautiful images. With this tool, users can easily generate and share their creations with the community. The project also incorporates MongoDB to store the data and Cloudinary to manage and optimize the images. The user interface is built using React, providing a simple and intuitive experience for users.',
-  },
-  {
-    id: 2,
-    name: 'Binary Search Tree Viewer',
-    path: 'bst-viewer',
-    preview: '/works/bst-visualizer/1.png',
-    images: [],
-    video: '/works/bst-visualizer/2.mp4',
-    github: 'https://github.com/alexisstdev/binary-search-tree-viewer',
-    description: 'Binary search tree visualizer and traversal algorithms viewer',
-    tags: ['C#', 'Windows Forms', 'SVG'],
-    longDescription:
-      'Binary search tree visualizer which allows users to add, remove and clear nodes from the tree, as well as visualize different traversal algorithms. This project was built using C# Windows Forms and SVGs to render the visual representation of the tree. The binary search tree data structure is widely used in computer science and this tool can help users to better understand its behavior and operations in an intuitive way.',
-  },
+	{
+		id: 1,
+		name: 'Alexis.AI - Image Generation',
+		path: 'alexisai',
+		category: 'Fullstack Application',
+		images: ['/works/alexis-ai/1.png', '/works/alexis-ai/2.png'],
+		preview: '/works/alexis-ai/1.png',
+		description: 'A tool for generate and share unique images with DALL-E.',
+		tags: ['React', 'Node.js', 'Express', 'MongoDB', 'DALL-E'],
+		github: 'https://github.com/alexisstdev/alexis.ai',
+		live: 'https://alexis-ai.vercel.app/',
+		longDescription:
+			'Fullstack MERN application, this personal project is an image generation tool that utilizes DALL-E to create unique and beautiful images. With this tool, users can easily generate and share their creations with the community. The project also incorporates MongoDB to store the data and Cloudinary to manage and optimize the images. The user interface is built using React, providing a simple and intuitive experience for users.',
+	},
+	{
+		id: 2,
+		name: 'Binary Search Tree Viewer',
+		path: 'bst-viewer',
+		preview: '/works/bst-visualizer/1.png',
+		images: [],
+		video: '/works/bst-visualizer/2.mp4',
+		github: 'https://github.com/alexisstdev/binary-search-tree-viewer',
+		description: 'Binary search tree visualizer and traversal algorithms viewer',
+		tags: ['C#', 'Windows Forms', 'SVG'],
+		longDescription:
+			'Binary search tree visualizer which allows users to add, remove and clear nodes from the tree, as well as visualize different traversal algorithms. This project was built using C# Windows Forms and SVGs to render the visual representation of the tree. The binary search tree data structure is widely used in computer science and this tool can help users to better understand its behavior and operations in an intuitive way.',
+	},
+	{
+		id: 3,
+		name: 'Weather App',
+		path: 'weatherapp',
+		preview: '/works/weatherapp/1.png',
+		images: ['/works/weatherapp/1.png', '/works/weatherapp/2.png'],
+		github: 'https://github.com/alexisstdev/weather-app',
+		description: 'Weather App that allows users to view forecasts for various locations.',
+		tags: ['React', 'Redux', 'Openweathermap', 'Google maps api'],
+		longDescription:
+			'User-friendly and intuitive weather application designed to provide accurate and up-to-date weather forecasts for locations around the world. With a sleek and modern interface, users can easily access weather information for their desired locations. Powered by React and Redux, this app offers a seamless user experience, allowing users to effortlessly navigate through the app and retrieve weather data. By leveraging the OpenWeatherMap API and Google Maps API, the app fetches precise and reliable weather information, including temperature, humidity, wind speed, and conditions for each location.',
+	},
 ];
