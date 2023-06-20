36fe4b7d2d3ed11ec6477636d182e1397488887c
alexisstdev <alexissanmiguel03@gmail.com>
2023-06-20 11:31:37 -0500

    details

--- src/Containers/AboutMe/AboutMe.jsx
+++ src/Containers/AboutMe/AboutMe.jsx
@@ -2,43 +2,48 @@ import './AboutMe.css';
 import { forwardRef } from 'react';
 
 function AboutMe({ goToRef, contactRef }, ref) {
-  return (
-    <div className='aboutme-container container' ref={ref} id='home'>
-      <div className='aboutme-content'>
-        <img src='me.jpg' alt='alexis-image' />
-        <div className='aboutme-info'>
-          <h1>
-            Hi, I'm <span>Alexis</span>
-          </h1>
-          <p>Software Developer</p>
-          <ul className='social-links'>
-            <li>
-              <a href='https://github.com/alexisstdev' target='_blank'>
-                <i className='fab fa-github'></i>
-              </a>
-            </li>
-            <li>
-              <a href='https://www.linkedin.com/in/alexisstdev/' target='_blank'>
-                <i className='fab fa-linkedin'></i>
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
+	return (
+		<div className='aboutme-container container' ref={ref} id='home'>
+			<div className='aboutme-content'>
+				<img src='me.jpg' alt='alexis-image' />
+				<div className='aboutme-info'>
+					<h1>
+						Hi, I'm <span>Alexis</span>
+					</h1>
+					<p>Software Developer</p>
+					<p>
+						Deeply passionate about creating user experiences and web applications.
+						Dedicated to delivering exceptional results and committed to maintaining
+						meticulous attention to detail in all my works.
+					</p>
+					<ul className='social-links'>
+						<li>
+							<a href='https://github.com/alexisstdev' target='_blank'>
+								<i className='fab fa-github'></i>
+							</a>
+						</li>
+						<li>
+							<a href='https://www.linkedin.com/in/alexisstdev/' target='_blank'>
+								<i className='fab fa-linkedin'></i>
+							</a>
+						</li>
+					</ul>
+				</div>
+			</div>
 
-      <div className='aboutme-buttons'>
-        <a
-          href='https://drive.google.com/file/d/1b8K0LEv_PwUaNEd-fPjfQHmAgZDDHzZ7/view?usp=sharing'
-          target='_blank'
-        >
-          <button className='btn btn-primary'>View resume</button>
-        </a>
-        <button className='btn btn-secondary' onClick={() => goToRef(contactRef)}>
-          Contact
-        </button>
-      </div>
-    </div>
-  );
+			<div className='aboutme-buttons'>
+				<a
+					href='https://drive.google.com/file/d/1b8K0LEv_PwUaNEd-fPjfQHmAgZDDHzZ7/view?usp=sharing'
+					target='_blank'
+				>
+					<button className='btn btn-primary'>View resume</button>
+				</a>
+				<button className='btn btn-secondary' onClick={() => goToRef(contactRef)}>
+					Contact
+				</button>
+			</div>
+		</div>
+	);
 }
 
 export default forwardRef(AboutMe);
--- src/Containers/Works/Works.jsx
+++ src/Containers/Works/Works.jsx
@@ -4,16 +4,16 @@ import { worksData } from '../../WorksData';
 import { forwardRef } from 'react';
 
 function Works({ props }, ref) {
-  return (
-    <section className='works container' ref={ref}>
-      <h2>Works</h2>
-      <div className='works-grid'>
-        {worksData.map((work) => (
-          <Work key={work.id} workData={work} />
-        ))}
-      </div>
-    </section>
-  );
+	return (
+		<section className='works container' ref={ref}>
+			<h2>Personal works</h2>
+			<div className='works-grid'>
+				{worksData.map((work) => (
+					<Work key={work.id} workData={work} />
+				))}
+			</div>
+		</section>
+	);
 }
 
 export default forwardRef(Works);
